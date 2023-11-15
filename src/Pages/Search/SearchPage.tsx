a7c18a4f8b7f2f8ecf2d35477ee04177b7ede7d9
Vijitha Gunta <vijitha.gunta@sjsu.edu>
2023-11-15 12:10:46 -0800

    Add follow user functionality

--- src/Pages/Search/SearchPage.tsx
+++ src/Pages/Search/SearchPage.tsx
@@ -3,24 +3,43 @@ import { TextField, Box, Button, Grid, Typography } from '@mui/material';
 import axios from 'axios';
 import SidebarComponent from '../../Components/SidebarComponent/SidebarComponent.tsx';
 import ProfileCard from '../../Components/ProfileCardComponent/ProfileCardComponent.tsx';
-import { useUserData, UserData } from '../../Contexts/UserDataContext.tsx';
 import CheckIcon from '@mui/icons-material/Check';
+import { UserData, useUserData } from '../../Contexts/UserDataContext.tsx';
 
 const SearchPage = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState<UserData[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
-  const { userData } = useUserData();
-  const [followedUsers, setFollowedUsers] = useState([]);
+  const { userData, setUserData } = useUserData();
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
 
-  const handleFollowUser = (userId) => {
-    setFollowedUsers(prev => [...prev, userId]);
-    // make an API call here to update the following status in backend
+  const handleFollowUser = async (userIdToFollow) => {
+    setIsLoading(true);
+    try {
+      await axios.post(`http://localhost:8050/api/users/${userData.userId}/follow/${userIdToFollow}`);
+      const updatedUserData = {
+        ...userData,
+        following: [...userData.following, userIdToFollow]
+      };
+      setUserData(updatedUserData);
+  
+      const updatedSearchResults = searchResults.map(profile => {
+        if (profile.userId === userIdToFollow) {
+          return { ...profile, followers: [...profile.followers, userData.userId] };
+        }
+        return profile;
+      });
+      setSearchResults(updatedSearchResults);
+      
+    } catch (error) {
+      const message = error.response && error.response.data ? error.response.data.message : error.message;
+      setErrorMessage(`Error following user: ${message}`);
+    }
+    setIsLoading(false);
   };
 
   const handleSearchSubmit = async () => {
@@ -29,20 +48,20 @@ const SearchPage = () => {
       }
     setIsLoading(true);
     setErrorMessage('');
-    setFollowedUsers([]);
     try {
-        const response = await axios.get(`http://localhost:8050/api/users/search?keyword=${searchQuery}`);
-        setSearchResults(response.data);
+        const searchRequest = axios.get(`http://localhost:8050/api/users/search?keyword=${searchQuery}`);
+        const userRequest = axios.get(`http://localhost:8050/api/users/getUser/${userData.userId}`);
+
+        const [userResponse, searchResponse] = await Promise.all([userRequest, searchRequest]);
+
+        setSearchResults(searchResponse.data);
+        setUserData(userResponse.data);
     } catch (error) {
         const message = error.response && error.response.data ? error.response.data.message : error.message;
         setErrorMessage(`Error fetching search results: ${message}`);
     }
     setIsLoading(false);
   };
-
-  console.log('my ' + userData.userId)
-  
-
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -66,27 +85,20 @@ const SearchPage = () => {
             {isLoading && <div>Loading...</div>}
             {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                {searchResults.length > 0 && searchResults.map( profile => {console.log('to follow ' + profile.userId)
-
+                {searchResults.length > 0 && searchResults.map( profile => {
                     return (<Box sx={{width: '75%', marginBottom: 2}}>
                         <ProfileCard profileData={profile} />
-                        {followedUsers.includes(profile.userId) ? (
-      <Box sx={{ display: 'flex', alignItems: 'center', marginTop: 1 }}>
-        <Typography>User Followed</Typography>
-        <CheckIcon color="success" />
-      </Box>
-    ) : (
-      <Button 
-        variant="contained" 
-        color="primary" 
-        onClick={() => handleFollowUser(profile.userId)}
-        sx={{ marginTop: 1 }}
-      >
-        Follow User
-      </Button>
-    )}
+                        {!userData?.following.includes(profile?.userId) && 
+                          <Button 
+                            variant="contained" 
+                            color="primary" 
+                            onClick={() => handleFollowUser(profile?.userId)}
+                            sx={{ marginTop: 1 }}
+                          >
+                            Follow User
+                          </Button>}
                     </Box>)
-})}
+                  })}
             </Box>
             </Box>
         </Grid>

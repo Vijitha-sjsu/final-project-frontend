95fff9e445955de73a26b3b3767052174c714634
Vijitha Gunta <vijitha.gunta@sjsu.edu>
2023-12-01 15:05:10 -0800

    fix url

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,3 @@
-// export const FOLLOW_SERVICE_BASE_URL = 'https://follow-service.default.svc.cluster.local:443';
-// export const POST_SERVICE_BASE_URL = 'https://post-service.default.svc.cluster.local:443';
-// export const FEED_SERVICE_BASE_URL = 'https://feed-service.default.svc.cluster.local:443';
-
-export const FOLLOW_SERVICE_BASE_URL = 'http://localhost:8050';
-export const POST_SERVICE_BASE_URL = 'http://localhost:8090';
-export const FEED_SERVICE_BASE_URL = 'https://feed-service.default.svc.cluster.local:443';
+export const FOLLOW_SERVICE_BASE_URL = 'https://146.235.233.3';
+export const POST_SERVICE_BASE_URL = 'https://146.235.233.3';
+export const FEED_SERVICE_BASE_URL = 'https://146.235.233.3';
